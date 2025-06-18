b50edb9585ba708bc7e81cf20627f45dacb2e357
sakthiprasad2004 <125357004+sakthiprasad2004@users.noreply.github.com>
2025-06-18 18:35:00 +0530

    [242] [Time Beats: 45.06%] [Memory Beats: 76.27%] - LeetPush

---
+++ string/character_frequency/Valid-Anagram.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        int[] freq =new int[26];
+
+        if(s.length() != t.length()) return false;
+        for(int i=0;i<s.length();i++){
+            freq[s.charAt(i)-'a']++;
+             freq[t.charAt(i)-'a']--;
+        }
+        for(int count : freq){
+            if(count != 0) return false; 
+        }
+        return true;
+    }
+}

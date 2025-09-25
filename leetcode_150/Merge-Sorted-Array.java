cc89123d0c4815032511ab8ed36533b2ebc6ce50
sakthiprasad2004 <125357004+sakthiprasad2004@users.noreply.github.com>
2025-09-25 18:45:10 +0530

    [88] [Time Beats: 28.84%] [Memory Beats: 27.45%] - LeetPush

---
+++ leetcode_150/Merge-Sorted-Array.java
@@ -0,0 +1,9 @@
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        for(int i=m,j=0;i<nums1.length;i++){
+            nums1[i] = nums2[j];
+            if(j< n) j++;
+    }
+    Arrays.sort(nums1);
+    }
+}
